fc3d5450df231b631a47a9b1a2e8b13e9f820947
elkinQuintero <78869413+elkinQuintero@users.noreply.github.com>
2022-07-27 17:14:16 -0500

    Add files via upload

---
+++ Python/EjercicioModulos/main.py
@@ -0,0 +1,6 @@
+import calculadora as cal
+
+print(f"La suma es: {cal.sumar(4,2)}")
+print(f"La resta es: {cal.restar(4,2)}")
+print(f"La división es: {cal.dividir(4,2)}")
+print(f"La multiplicación es: {cal.multiplicar(4,2)}")
